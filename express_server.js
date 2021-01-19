ff1632e5baaa95d67e640791cf2f25c5ff791b94
Anto Ter-Mesropian <antotm99@gmail.com>
2021-01-19 01:03:18 +0000

    added POST

--- express_server.js
+++ express_server.js
@@ -4,10 +4,19 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+  let result = Math.random().toString(36).substring(2, 8);
+  return result;
+}
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -25,6 +34,10 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -33,4 +46,9 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: href='#'};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
